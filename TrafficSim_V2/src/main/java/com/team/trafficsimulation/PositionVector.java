dda1821c22797be9381ffbaeadb46fdb950f8ff8
Caspar <cas0305@outlook.de>
2025-12-11 21:17:40 +0100

    Vehicle, TrafficLight and PositionVektor added

---
+++ TrafficSim_V2/src/main/java/com/team/trafficsimulation/PositionVector.java
@@ -0,0 +1,20 @@
+package com.team.trafficsimulation;
+
+public class PositionVector {
+    private final double x;
+    private final double y;
+
+    public PositionVector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+}
---
+++ TrafficSim_V2/src/main/java/com/team/trafficsimulation/Vehicle.java
@@ -0,0 +1,46 @@
+package com.team.trafficsimulation;
+
+import org.eclipse.sumo.libtraci.StringVector;
+
+public class Vehicle {
+
+    //Attributes and Constructor
+    private final String id;
+    public Vehicle(String id) {
+        this.id = id;
+    }
+    public String getId() {
+        return this.id;
+    }
+
+    //Getter methods
+    public double getSpeed() {
+        return org.eclipse.sumo.libtraci.Vehicle.getSpeed(this.id);
+    }
+
+    public PositionVector getPositionVector(){
+        return new PositionVector(org.eclipse.sumo.libtraci.Vehicle.getPosition(this.id).getX(), org.eclipse.sumo.libtraci.Vehicle.getPosition(this.id).getY());
+    }
+
+    public String getEdgeId() {
+        return org.eclipse.sumo.libtraci.Vehicle.getRoadID(this.id);
+    }
+
+
+    //Setter methods
+    public void setSpeed(double speed) {
+        org.eclipse.sumo.libtraci.Vehicle.setSpeed(this.id, speed);
+    }
+
+    //necessary?
+    public void changeLane(int laneIndex, double duration) {
+        org.eclipse.sumo.libtraci.Vehicle.changeLane(this.id, laneIndex, duration);
+    }
+
+    public void setRoute(StringVector edgeIds) {
+        org.eclipse.sumo.libtraci.Vehicle.setRoute(this.id, edgeIds);
+    }
+
+    //setroute overload?
+}
+
