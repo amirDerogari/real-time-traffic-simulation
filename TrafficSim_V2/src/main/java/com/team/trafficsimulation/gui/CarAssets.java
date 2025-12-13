ed0adc0582eb07cdb1d2df9054fa489a65543d6d
Mojtaba <bagheripoor.mojtaba@gmail.com>
2025-12-13 16:11:11 +0100

    feat: moving cars demo (JavaFX)

---
+++ TrafficSim_V2/src/main/java/com/team/trafficsimulation/gui/CarAssets.java
@@ -0,0 +1,19 @@
+package com.team.trafficsimulation.gui;
+
+import java.util.List;
+
+public final class CarAssets {
+    private CarAssets() {}
+
+    // Your demo order (change order here whenever you want)
+    public static final List<String> ORDERED = List.of(
+            "/images/cars/YellowCar.png",
+            "/images/cars/BlackCar.png",
+            "/images/cars/BlueCar.png",
+            "/images/cars/GreenCar.png",
+            "/images/cars/PinkCar.png",
+            "/images/cars/PurpleCar.png",
+            "/images/cars/RedCar.png",
+            "/images/cars/WhiteCar.png"
+    );
+}
